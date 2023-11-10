f07b1878b9bd9322985984fd1601c87d8f49712f
Ercanincee <132362073+Ercanincee@users.noreply.github.com>
2023-11-10 19:06:05 +0300

    Add project files.

---
+++ tryparse/Program.cs
@@ -0,0 +1,16 @@
+﻿// Girilen metnin harf sayısını Yazıyor
+string girilenmetin = "";
+Console.WriteLine("Bir Metin Giriniz");
+do
+{
+    girilenmetin = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(girilenmetin)) {
+        Console.WriteLine("Gecerli Bir Deger Giriniz");
+        girilenmetin= Console.ReadLine();
+    }else { break; }
+} while (string.IsNullOrEmpty(girilenmetin));
+char[] variables =girilenmetin.ToCharArray();
+Console.WriteLine(variables.Length);
+
+
